a37179419380170c45fef02af1dae9373c16986a
Tom Constant <t.j.constant@gmail.com>
2018-08-28 20:43:42 +0100

    bug fix to allow more than one gameweek

--- R/get_league_history.R
+++ R/get_league_history.R
@@ -1,6 +1,6 @@
-get_league_history <- function(league_id, start_gw = NULL){
+get_league_history <- function(league_id, start_gw = 1){
 
-  league_details <- get_league(league_id)
+  league_details <- lapply(league_id, get_league) %>% dplyr::bind_rows()
 
   history <- tibble::tibble()
 
@@ -9,8 +9,8 @@ get_league_history <- function(league_id, start_gw = NULL){
                                 get_manager_history(player_id, start_gw))
   }
 
-  dplyr::bind_cols(league_name = league_details$league_name,
-                   league_id = league_details$league_id,
+  dplyr::bind_cols(league_name = rep(league_details$league_name,length(history$id)/length(league_details$league_name)),
+                   league_id = rep(league_details$league_id,length(history$id)/length(league_details$league_name)),
                    history)
 
 }
--- R/update_league_history.R
+++ R/update_league_history.R
@@ -1,10 +1,13 @@
 update_league_history <- function(filename){
-  data <- readr::read_csv(filename)
+
+  if(file.exists(filename)){
+    data <- readr::read_csv(filename)
 
   if(get_current_gw() > max(data$gw)){
     new_data <- get_league_history(unique(data$league_id), get_current_gw())
     readr::write_csv(new_data, filename, append = TRUE)
+  }else{
+    stop("File Not Found")
+    }
   }
-
-
 }
